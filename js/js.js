0b4fa6d9988797352722a73078b6f1d451c939a5
Justin H <jhidalgo2@my.wctc.edu>
2022-02-07 03:44:11 -0600

    finished (No verification or submits)

--- js/js.js
+++ js/js.js
@@ -1,4 +1,35 @@
 $(document).ready(function () {
+    hideALl()
+    $(window).scrollTop(0);
+    dateFront()
+
+    $('.nextButton').on('click',nextTab);
+    $('.backButton').on('click',previousTab);
+    $('#moreWork').on('click',WorkExperienceForm);
+
+    $(document).change(function(){
+        hideUnnecessary();
+    }
+    )
+
+})
+let housingValue = $('#housingChoice').val()
+let relativeOfEmployee = $('#spouseApplication').val()
+let previousNames = $('#previousNamesYN').val()
+let page = "#page"
+let currentPageNumber = 1;
+let currentPageID = page + currentPageNumber
+let formCount = 1;
+let currentForm = "form"+ formCount
+let workedBefore = $('#workedBefore').val()
+
+function hideALl(){
+    $('#spouseOrFriendOfEmployee').hide()
+    $('#inputRV').hide()
+    $('#previousNamesUsed').hide()
+    $('#previouslyWorkedBefore').hide()
+}
+function dateFront() {
     var today = new Date();
     var dd = String(today.getDate()).padStart(2, '0');
     var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
@@ -6,16 +37,47 @@ $(document).ready(function () {
 
     today = mm + '/' + dd + '/' + yyyy;
     $('#currentDate').html(today);
+}
 
-    $('.nextButton').on('click',nextTab);
-    $('.backButton').on('click',previousTab);
+function hideUnnecessary(){
+    housingValue = $('#housingChoice').val()
+    relativeOfEmployee = $('#spouseApplication').val()
+    previousNames = $('#previousNamesYN').val()
+    workedBefore = $('#workedBefore').val()
+    if (workedBefore === "yes"){
+        $('#previouslyWorkedBefore').show()
+    }
+    else{
+        $('#previouslyWorkedBefore').hide()
+    }
 
-})
-let page = "#page"
-let currentPageNumber = 1;
-let currentPageID = page + currentPageNumber
+    if (housingValue === "RV"){
+        $('#inputRV').show();
+
+    }
+    else{
+        $('#inputRV').hide()
+    }
+
+    if (previousNames === "yes"){
+        $('#previousNamesUsed').show();
+
+    }
+    else{
+        $('#previousNamesUsed').hide()
+    }
+
+    if (relativeOfEmployee === "yes"){
+        $('#spouseOrFriendOfEmployee').show();
+    }
+    else{
+        $('#spouseOrFriendOfEmployee').hide();
+    }
+}
 
 function nextTab(){
+
+    $(window).scrollTop(0);
     $(currentPageID).hide();
     currentPageNumber += 1;
     currentPageID = page + currentPageNumber
@@ -25,11 +87,23 @@ function nextTab(){
 }
 
 function previousTab(){
+    $(window).scrollTop(0);
     $(currentPageID).hide();
     currentPageNumber -= 1;
     currentPageID = page + currentPageNumber
     $(currentPageID).tab('show')
     $(currentPageID).show()
+
+}
+
+function WorkExperienceForm(){
+    console.log(currentForm)
+    let form = document.getElementById(currentForm)
+    let form_dup = form.cloneNode(true);
+    formCount+=1
+    form_dup.id = "form"+formCount
+    form.append(form_dup)
+    currentForm = 'form'+ formCount
 
 }
 
